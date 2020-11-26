ed10f4f08d14b69920816f5e3d5f29855bc586a2
rocionadal <rocioonadal@gmail.com>
2020-11-26 12:41:19 +0100

    created controller for ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'ingredients/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails do
     resources :ingredients
