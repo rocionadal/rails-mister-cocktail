a28bcc1ab0e54b34396e5e9246691450e7a20ebf
rocionadal <rocioonadal@gmail.com>
2020-11-26 18:50:49 +0100

    refactoring code

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,16 +1,16 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :destroy]
+  before_action :set_cocktail, only: [:new, :create]
+
   def show
-    @dose = Dose.find(params[:id])
   end
 
   def new
     @dose = Dose.new
-    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
     @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params["cocktail_id"])
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
@@ -21,10 +21,18 @@ class DosesController < ApplicationController
 
   def destroy
     @dose.destroy
-    redirect_to cocktails_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
