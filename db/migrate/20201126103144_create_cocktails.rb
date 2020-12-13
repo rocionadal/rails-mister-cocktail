6296a7f8465c8894b68787b48ef2724f275123fd
rocionadal <rocioonadal@gmail.com>
2020-12-13 09:27:53 +0100

    migration for db

--- db/migrate/20201126103144_create_cocktails.rb
+++ db/migrate/20201126103144_create_cocktails.rb
@@ -2,7 +2,6 @@ class CreateCocktails < ActiveRecord::Migration[6.0]
   def change
     create_table :cocktails do |t|
       t.string :name
-      t.string :description
       t.string :image_url
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_150931) do
+ActiveRecord::Schema.define(version: 2020_12_13_082456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,10 +38,10 @@ ActiveRecord::Schema.define(version: 2020_11_27_150931) do
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
-    t.string "description"
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "doses", force: :cascade do |t|
